9065088058a3935c965ce328665aaa6611640309
Erik <erik.monroy.an@gmail.com>
2023-09-12 16:20:51 -0600

    Validacion con middleware JOI

--- components/auth/network.js
+++ components/auth/network.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const controller = require('./controller');
 const response = require('../../network/response');
-
 const router = express.Router();
 
+// Validaciones
+const { validatorLogin } = require('./validators')
+const { loginSchema } = require('./schema-validator');
+
 // Rutas
-router.post('/login', login)
-
-
-// Funciones
+router.post('/login', validatorLogin(loginSchema, "body"), login)
+    // Funciones
 function login(req, res, next) {
     const { username, password } = req.body;
     controller.login(username, password)
---
+++ components/auth/validators.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+const errors = require('../../utils/error');
+
+function validatorLogin(schema, property) {
+    return (req, res, next) => {
+        const data = req[property];
+        const { error } = schema.validate(data);
+        if (error) {
+            console.log(error);
+            throw errors(error.details[0].message, 501);
+        } else {
+            next();
+        }
+    }
+}
+
+
+module.exports = {
+    validatorLogin
+};
